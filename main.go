502eecf0918a678c1fce40de8a5151449541315f
Tryggvi Larusson <tryggvi.larusson@netapp.com>
2019-01-17 10:18:30 +0000

    test2

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example test"
+	title := "Jenkins X golang http example test2"
 
 	from := ""
 	if r.URL != nil {
